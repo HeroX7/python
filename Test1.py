09c69b3785b24093fe0824716fd029d7b95c30a6
HeroX7 <moh.hageb@gmail.com>
2020-06-24 23:21:07 +0300

    Hello World and multiline new line

--- Test1.py
+++ Test1.py
@@ -1,3 +1,6 @@
-print("Hello")
-print("Hi")
-print("last")
+#this is a hello world application
+print('Hello World !')
+print("Hello World !!")
+#multiline
+print('''this is multi line Hello World 
+how are you today ?''')
